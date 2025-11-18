e486ece8951e8713b796a8c3bf37a09e4384c83e
amirahassandev <amirahassansobhi@gmail.com>
2025-11-18 02:59:17 +0200

    C#Notes

--- TestCodes/obj/Debug/net9.0/TestCodes.AssemblyInfo.cs
+++ TestCodes/obj/Debug/net9.0/TestCodes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2175b5cff0e8cd8eaaec9c40dec794277242a5df")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
